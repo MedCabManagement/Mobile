f25990e1f4fbd3553ad31821fce9350772abc367
Teyzar <thomasandrewzaragoza@gmail.com>
2022-04-26 17:22:22 +0800

    chore(list): replace to list file dart

---
+++ lib/views/patient/list.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:patient/models/patient.dart';
+import 'package:patient/models/user.dart';
+import 'package:patient/views/patient/Profile.dart';
+import 'package:patient/views/patient/create.dart';
+import 'package:patient/views/home.dart';
+
+class PatientList extends StatefulWidget {
+  final User user;
+  const PatientList(this.user, {Key? key}) : super(key: key);
+
+  @override
+  State<PatientList> createState() => _PatientListState();
+}
+
+class _PatientListState extends State<PatientList> {
+  final patient = Patient();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: TextButton(
+          child: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Home(widget.user),
+              ),
+            );
+          },
+        ),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+        title: const Text("Patient List"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.person_add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.green,
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => CreatePatient(widget.user),
+          ));
+        },
+      ),
+      body: getPatients(),
+    );
+  }
+
+  Widget getPatients() {
+    return FutureBuilder(
+      future: patient.get(widget.user.token.toString()),
+      builder: (context, AsyncSnapshot<List> snapshot) {
+        final connectionDone = snapshot.connectionState == ConnectionState.done;
+        if (connectionDone && snapshot.hasData) {
+          final patients = snapshot.data!;
+          return ListView.builder(
+              padding: const EdgeInsets.all(5),
+              itemCount: patients.length,
+              itemBuilder: (context, index) {
+                final patient = patients[index];
+                return Card(
+                  color: const Color.fromARGB(255, 250, 247, 237),
+                  elevation: 20,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.all(5),
+                    minLeadingWidth: 0,
+                    leading: const CircleAvatar(
+                        child: Icon(
+                          Icons.person,
+                          color: Colors.white,
+                        ),
+                        radius: 25,
+                        backgroundColor: Colors.greenAccent),
+                    title: Text(
+                      patient['name'].toString(),
+                      style: const TextStyle(
+                          fontFamily: "OpenSans",
+                          fontSize: 17,
+                          color: Colors.black),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (BuildContext context) => Profile(
+                          id: patient['_id'],
+                          name: patient['name'],
+                          age: patient['age'],
+                          token: widget.user.token.toString(),
+                        ),
+                      ));
+                    },
+                  ),
+                );
+              });
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
