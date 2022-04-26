4204e941d6ab1b0f9a7feb0509e916ead1d3674b
Teyzar <thomasandrewzaragoza@gmail.com>
2022-04-26 17:21:24 +0800

    chore(login): add validation on input field

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -14,6 +14,8 @@ class _LoginState extends State<Login> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   bool passwordObscured = true;
   bool inputFieldEnabled = true;
   bool changePassword = false;
@@ -22,6 +24,7 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.green,
         title: const Text("MDC Management Application",
             style: TextStyle(
@@ -29,87 +32,99 @@ class _LoginState extends State<Login> {
                 color: Colors.white,
                 fontWeight: FontWeight.bold)),
       ),
-      body: ListView(
-        padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
-        children: <Widget>[
-          CircleAvatar(
-            child: Image.asset('assets/app_logo_icon.png'),
-            radius: 100,
-          ),
-          const SizedBox(height: 30.0),
-          TextField(
-            controller: emailController,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              labelText: "E-mail",
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
-              prefixIcon: Icon(Icons.email_outlined),
-            ),
-            enabled: inputFieldEnabled,
-          ),
-          const SizedBox(height: 15.0),
-          TextField(
-            controller: passwordController,
-            decoration: InputDecoration(
-              labelText: "Password",
-              border: const OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
-              prefixIcon: const Icon(Icons.lock),
-              suffixIcon: IconButton(
-                icon: Icon(
-                  passwordObscured
-                      ? Icons.visibility_rounded
-                      : Icons.visibility_off_rounded,
-                ),
-                onPressed: () {
-                  setState(() {
-                    passwordObscured = !passwordObscured;
-                  });
-                },
+      body: Form(
+          key: _formKey,
+          child: ListView(
+            padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
+            children: <Widget>[
+              CircleAvatar(
+                child: Image.asset('assets/app_logo_icon.png'),
+                radius: 100,
               ),
-            ),
-            enabled: inputFieldEnabled,
-            obscureText: passwordObscured,
-          ),
-          const SizedBox(height: 20.0),
-          Container(
-              height: 50,
-              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-              child: ElevatedButton(
-                  child: const Text('Login'),
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  onPressed: inputFieldEnabled
-                      ? () {
-                          FocusScope.of(context).requestFocus(FocusNode());
+              const SizedBox(height: 30.0),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) return 'Please Enter Email';
+                },
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: "E-mail",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  prefixIcon: Icon(Icons.email_outlined),
+                ),
+                enabled: inputFieldEnabled,
+              ),
+              const SizedBox(height: 15.0),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) return 'Please Enter Password';
+                },
+                controller: passwordController,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      passwordObscured
+                          ? Icons.visibility_rounded
+                          : Icons.visibility_off_rounded,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        passwordObscured = !passwordObscured;
+                      });
+                    },
+                  ),
+                ),
+                enabled: inputFieldEnabled,
+                obscureText: passwordObscured,
+              ),
+              const SizedBox(height: 20.0),
+              Container(
+                  height: 50,
+                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                  child: ElevatedButton(
+                      child: const Text('Login'),
+                      style: ElevatedButton.styleFrom(primary: Colors.green),
+                      onPressed: inputFieldEnabled
+                          ? () {
+                              if (_formKey.currentState!.validate()) {
+                                FocusScope.of(context)
+                                    .requestFocus(FocusNode());
 
-                          setState(() {
-                            inputFieldEnabled = false;
-                          });
+                                setState(() {
+                                  inputFieldEnabled = false;
+                                });
 
-                          Future((() async {
-                            final email = emailController.text;
-                            final password = passwordController.text;
-                            final statusCode =
-                                await user.login(email, password);
+                                Future((() async {
+                                  final email = emailController.text;
+                                  final password = passwordController.text;
+                                  final statusCode =
+                                      await user.login(email, password);
 
-                            if (statusCode == 200) {
-                              Navigator.of(context).popUntil(
-                                  (route) => Navigator.of(context).canPop());
-                              Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => Home(user),
-                              ));
-                            } else {
-                              setState(() {
-                                inputFieldEnabled = true;
-                              });
+                                  if (statusCode == 200) {
+                                    Navigator.of(context).popUntil((route) =>
+                                        Navigator.of(context).canPop());
+                                    Navigator.of(context)
+                                        .push(MaterialPageRoute(
+                                      builder: (context) => Home(user),
+                                    ));
+                                  } else {
+                                    setState(() {
+                                      inputFieldEnabled = true;
+                                    });
+                                  }
+                                }));
+                              }
                             }
-                          }));
-                        }
-                      : null)),
-          const SizedBox(height: 50),
-        ],
-      ),
+                          : null)),
+              const SizedBox(height: 50),
+            ],
+          )),
     );
   }
 }
