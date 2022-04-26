2df6f43df86ee0b3724154413dde81f317f2ff7e
Teyzar <thomasandrewzaragoza@gmail.com>
2022-04-26 17:22:39 +0800

    chore(profile): update profile dart screen

--- lib/views/patient/Profile.dart
+++ lib/views/patient/Profile.dart
@@ -1,14 +1,17 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:patient/models/patient.dart';
 import 'package:patient/views/home.dart';
 
 class Profile extends StatefulWidget {
-  final String name, id;
+  final String name, id, token;
   final int age;
   const Profile(
-      {required this.id, required this.name, required this.age, Key? key})
+      {required this.id,
+      required this.name,
+      required this.age,
+      required this.token,
+      Key? key})
       : super(key: key);
 
   @override
@@ -35,11 +38,6 @@ class _ProfileState extends State<Profile> {
       body: ListView(
         padding: const EdgeInsets.all(15),
         children: [
-          const CircleAvatar(
-            child: Icon(Icons.person, size: 70, color: Colors.white),
-            radius: 60,
-            backgroundColor: Colors.greenAccent,
-          ),
           const SizedBox(height: 10),
           const Text("Name",
               style: TextStyle(
@@ -68,17 +66,68 @@ class _ProfileState extends State<Profile> {
                 leading: Text(widget.age.toString(),
                     style: const TextStyle(fontSize: 17)),
               )),
-          const SizedBox(height: 5),
-          TextButton.icon(
-              onPressed: () async {
-                final res = await patient.delete(widget.id);
+          const SizedBox(height: 10),
 
-                if (res == 200) {
-                  Navigator.pop(context, true);
+          Card(
+            child: FutureBuilder(
+              future: patient.getMedications(widget.id, widget.token),
+              builder: (context, AsyncSnapshot<List> snapshot) {
+                final connectionDone =
+                    snapshot.connectionState == ConnectionState.done;
+                if (connectionDone && snapshot.hasData) {
+                  final medicines = snapshot.data!;
+                  // print(medicines);
+                  return ListView.builder(
+                    padding: const EdgeInsets.all(5),
+                    itemCount: medicines.length,
+                    itemBuilder: (context, index) {
+                      final medicine = medicines[index];
+                      return Card(
+                        color: const Color.fromARGB(255, 250, 247, 237),
+                        elevation: 20,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: ListTile(
+                          contentPadding: const EdgeInsets.all(5),
+                          minLeadingWidth: 0,
+                          leading: const CircleAvatar(
+                              child: Icon(
+                                Icons.person,
+                                color: Colors.white,
+                              ),
+                              radius: 25,
+                              backgroundColor: Colors.greenAccent),
+                          title: Text(
+                            medicine['name'].toString(),
+                            style: const TextStyle(
+                                fontFamily: "OpenSans",
+                                fontSize: 17,
+                                color: Colors.black),
+                          ),
+                          onTap: () {},
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
                 }
               },
-              icon: const Icon(Icons.delete, color: Colors.red),
-              label: const Text("Delete", style: TextStyle(color: Colors.red)))
+            ),
+          ),
+
+          // TextButton.icon(
+          //     onPressed: () async {
+          //       final res = await patient.delete(widget.id);
+
+          //       if (res == 200) {
+          //         Navigator.pop(context, true);
+          //       }
+          //     },
+          //     icon: const Icon(Icons.delete, color: Colors.red),
+          //     label: const Text("Delete", style: TextStyle(color: Colors.red)))
         ],
       ),
     );
