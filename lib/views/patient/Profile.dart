9736b921f231e10de22eae5a7ce632f50bf365f3
Thomas Andrew Zaragoza <thomasandrewzaragoza@gmail.com>
2022-04-27 09:31:37 +0800

    feat(profile): update profile dart

--- lib/views/patient/Profile.dart
+++ lib/views/patient/Profile.dart
@@ -1,15 +1,19 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:patient/models/medicine.dart';
 import 'package:patient/models/patient.dart';
 import 'package:patient/views/home.dart';
 
 class Profile extends StatefulWidget {
   final String name, id, token;
+  final String medication;
   final int age;
   const Profile(
       {required this.id,
       required this.name,
       required this.age,
+      required this.medication,
       required this.token,
       Key? key})
       : super(key: key);
@@ -23,6 +27,7 @@ class _ProfileState extends State<Profile> {
 
   @override
   Widget build(BuildContext context) {
+    print(widget.medication);
     return Scaffold(
       backgroundColor: Colors.green[50],
       appBar: AppBar(
@@ -67,67 +72,21 @@ class _ProfileState extends State<Profile> {
                     style: const TextStyle(fontSize: 17)),
               )),
           const SizedBox(height: 10),
-
           Card(
-            child: FutureBuilder(
-              future: patient.getMedications(widget.id, widget.token),
-              builder: (context, AsyncSnapshot<List> snapshot) {
-                final connectionDone =
-                    snapshot.connectionState == ConnectionState.done;
-                if (connectionDone && snapshot.hasData) {
-                  final medicines = snapshot.data!;
-                  // print(medicines);
-                  return ListView.builder(
-                    padding: const EdgeInsets.all(5),
-                    itemCount: medicines.length,
-                    itemBuilder: (context, index) {
-                      final medicine = medicines[index];
-                      return Card(
-                        color: const Color.fromARGB(255, 250, 247, 237),
-                        elevation: 20,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        child: ListTile(
-                          contentPadding: const EdgeInsets.all(5),
-                          minLeadingWidth: 0,
-                          leading: const CircleAvatar(
-                              child: Icon(
-                                Icons.person,
-                                color: Colors.white,
-                              ),
-                              radius: 25,
-                              backgroundColor: Colors.greenAccent),
-                          title: Text(
-                            medicine['name'].toString(),
-                            style: const TextStyle(
-                                fontFamily: "OpenSans",
-                                fontSize: 17,
-                                color: Colors.black),
-                          ),
-                          onTap: () {},
-                        ),
-                      );
-                    },
-                  );
-                } else {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
+            child: ListTile(
+              leading: Text(widget.medication),
             ),
           ),
+          TextButton.icon(
+              onPressed: () async {
+                final res = await patient.delete(widget.id);
 
-          // TextButton.icon(
-          //     onPressed: () async {
-          //       final res = await patient.delete(widget.id);
-
-          //       if (res == 200) {
-          //         Navigator.pop(context, true);
-          //       }
-          //     },
-          //     icon: const Icon(Icons.delete, color: Colors.red),
-          //     label: const Text("Delete", style: TextStyle(color: Colors.red)))
+                if (res == 200) {
+                  Navigator.pop(context, true);
+                }
+              },
+              icon: const Icon(Icons.delete, color: Colors.red),
+              label: const Text("Delete", style: TextStyle(color: Colors.red)))
         ],
       ),
     );
