08921045d7a421370205890eb25790a11706c99c
Teyzar <thomasandrewzaragoza@gmail.com>
2022-04-27 14:05:38 +0800

    chore(medicine.dart): create API class for med

--- lib/models/medicine.dart
+++ lib/models/medicine.dart
@@ -1,21 +1,31 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+import 'package:patient/models/medication.dart';
+
 class Medicine {
   String? id;
   String? name;
-  int? container;
-  int? quantity;
+  String? quantity;
+  String? container;
 
-  Medicine({
-    required this.id,
-    required this.name,
-    required this.container,
-    required this.quantity,
-  });
-  factory Medicine.fromJson(Map<String, dynamic> json) {
-    return Medicine(
-      id: json['_id'],
-      name: json['name'],
-      container: json['container'],
-      quantity: json['quantity'],
-    );
+  Medicine();
+
+  final domain = "medcab-rrc.herokuapp.com";
+  Future<List> get(String _token) async {
+    final url = Uri.https(domain, "/medicines");
+
+    final headers = <String, String>{
+      HttpHeaders.contentTypeHeader: ContentType.json.toString(),
+      HttpHeaders.acceptHeader: ContentType.json.toString(),
+      HttpHeaders.authorizationHeader: "Bearer $_token"
+    };
+
+    final response = await http.get(url, headers: headers);
+
+    final data = jsonDecode(response.body);
+
+    return data;
   }
 }
