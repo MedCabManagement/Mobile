e0206b4c7520dd8e5e424346f309707dfe6835e2
Teyzar <thomasandrewzaragoza@gmail.com>
2022-04-28 16:24:37 +0800

    feat: change directory

---
+++ lib/models/patient/patientInfo.dart
@@ -0,0 +1,24 @@
+import 'package:patient/models/medication/medication.dart';
+
+class PatientInfo {
+  String? id;
+  String? name;
+  int? age;
+  List<Medication>? medications;
+
+  PatientInfo(
+      {required this.id,
+      required this.name,
+      required this.age,
+      required this.medications});
+
+  factory PatientInfo.fromJson(Map<String, dynamic> json) {
+    return PatientInfo(
+        id: json['_id'],
+        name: json['name'],
+        age: json['age'],
+        medications: (json['medications'] as List<dynamic>)
+            .map((e) => Medication.fromJson(e))
+            .toList());
+  }
+}
