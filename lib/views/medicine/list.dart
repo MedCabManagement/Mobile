6cceecbd833f8aefb9c868b9aa592a6f8ad00f3a
Thomas Andrew Zaragoza <thomasandrewzaragoza@gmail.com>
2022-05-06 04:50:56 +0800

    chore(medInfo.dart): update and implement medInfo

--- lib/views/medicine/list.dart
+++ lib/views/medicine/list.dart
@@ -3,6 +3,7 @@ import 'package:patient/models/medicine/medicine.dart';
 import 'package:patient/models/user.dart';
 import 'package:patient/views/home.dart';
 import 'package:patient/views/medicine/create.dart';
+import 'package:patient/views/medicine/medInfo.dart';
 
 class MedicineList extends StatefulWidget {
   final User user;
@@ -13,87 +14,111 @@ class MedicineList extends StatefulWidget {
 }
 
 class _MedicineListState extends State<MedicineList> {
-
   final medicine = Medicine();
+
+  fetchMeds() {
+    return medicine.get(widget.user.token.toString());
+  }
+
+  Future<void> refreshList() async {
+    await Future.delayed(const Duration(milliseconds: 0));
+    setState(() {
+      fetchMeds();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[50],
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        leading: TextButton(
+        backgroundColor: Colors.green[50],
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          leading: TextButton(
+            child: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Home(widget.user),
+                ),
+              );
+            },
+          ),
+          backgroundColor: Colors.green,
+          centerTitle: true,
+          title: const Text("Medicines"),
+        ),
+        floatingActionButton: FloatingActionButton(
           child: const Icon(
-            Icons.arrow_back,
+            Icons.add,
             color: Colors.white,
           ),
+          backgroundColor: Colors.green,
           onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (context) => Home(widget.user),
-              ),
-            );
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => CreateMedicine(widget.user),
+            ));
           },
         ),
-        backgroundColor: Colors.green,
-        centerTitle: true,
-        title: const Text("Medicines"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
-        backgroundColor: Colors.green,
-        onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => CreateMedicine(widget.user),
-          ));
-        },
-      ),
-      body: getMedicines(),
-    );
+        body: RefreshIndicator(
+          onRefresh: refreshList,
+          child: getMedicines(),
+        ));
   }
 
   Widget getMedicines() {
     return FutureBuilder(
-      future: medicine.get(widget.user.token.toString()),
+      future: fetchMeds(),
       builder: (context, AsyncSnapshot<List> snapshot) {
         final connectionDone = snapshot.connectionState == ConnectionState.done;
         if (connectionDone && snapshot.hasData) {
-          final patients = snapshot.data!;
+          final medicines = snapshot.data!;
           return ListView.builder(
               padding: const EdgeInsets.all(5),
-              itemCount: patients.length,
+              itemCount: medicines.length,
               itemBuilder: (context, index) {
-                final patient = patients[index];
+                final medicine = medicines[index];
                 return Card(
-                  color: const Color.fromARGB(255, 196, 234, 243),
-                  elevation: 20,
+                  color: const Color.fromARGB(255, 190, 228, 236),
+                  elevation: 10,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20)),
                   child: ListTile(
-                    contentPadding: const EdgeInsets.fromLTRB(20,5,5,5),
+                    trailing: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: const [
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              size: 17,
+                              color: Colors.black,
+                            )
+                          ],
+                        )),
+                    contentPadding: const EdgeInsets.fromLTRB(20, 5, 5, 5),
                     minLeadingWidth: 0,
                     leading: Text(
-                      patient['name'].toString(),
+                      medicine['name'].toString(),
                       style: const TextStyle(
                           fontFamily: "OpenSans",
                           fontSize: 17,
                           color: Colors.black),
                     ),
                     onTap: () async {
-                      // final _dispatient = await _patient.getPatient(
-                      //     patient['_id'], widget.user.token.toString());
-
-                      // Navigator.of(context).push(MaterialPageRoute(
-                      //   builder: (BuildContext context) => Profile(
-                      //     id: patient['_id'],
-                      //     name: patient['name'],
-                      //     age: patient['age'],
-                      //     token: widget.user.token.toString(),
-                      //   ),
-                      // ));
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (BuildContext context) => MedInfo(
+                            id: medicine['_id'],
+                            name: medicine['name'],
+                            quantity: medicine['quantity'],
+                            container: medicine['container'],
+                            user: widget.user),
+                      ));
                     },
                   ),
                 );
