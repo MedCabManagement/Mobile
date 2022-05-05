dcdaaa58653cc0438483d7479f7a8ab96136a12c
Thomas Andrew Zaragoza <thomasandrewzaragoza@gmail.com>
2022-05-06 04:49:21 +0800

    feat(patient.dart) : add delete Medication

--- lib/models/patient/patient.dart
+++ lib/models/patient/patient.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 import 'package:patient/models/medication/medication.dart';
 import 'package:patient/models/schedule/schedule.dart';
-import 'package:patient/views/patient/medication/schedules/view.dart';
+import 'package:patient/views/patient/medication/schedules/add.dart';
 
 const domain = "medcab-rrc.herokuapp.com";
 
@@ -61,14 +62,15 @@ class Patient {
     return data;
   }
 
-  Future<int> delete(String id) async {
+  Future<int> delete(String id, String _token) async {
     final String endPoint = "/patients/$id";
 
     final url = Uri.https(domain, endPoint);
 
     final headers = <String, String>{
       HttpHeaders.contentTypeHeader: ContentType.json.toString(),
-      HttpHeaders.acceptHeader: ContentType.json.toString()
+      HttpHeaders.acceptHeader: ContentType.json.toString(),
+      HttpHeaders.authorizationHeader: "Bearer $_token"
     };
 
     final response = await http.delete(url, headers: headers);
@@ -76,6 +78,31 @@ class Patient {
     if (response.statusCode == HttpStatus.ok) {
       Fluttertoast.showToast(
         msg: 'Deleted Successfuly',
+        toastLength: Toast.LENGTH_LONG,
+      );
+    }
+
+    return response.statusCode;
+  }
+
+  Future<int> edit(
+      String patientID, String _name, int _age, String _token) async {
+    final String endPoint = "/patients/$patientID";
+
+    final url = Uri.https(domain, endPoint);
+
+    final headers = <String, String>{
+      HttpHeaders.contentTypeHeader: ContentType.json.toString(),
+      HttpHeaders.acceptHeader: ContentType.json.toString(),
+      HttpHeaders.authorizationHeader: "Bearer $_token"
+    };
+
+    final response = await http.patch(url,
+        headers: headers, body: jsonEncode({"name": _name, "age": _age}));
+
+    if (response.statusCode == HttpStatus.ok) {
+      Fluttertoast.showToast(
+        msg: 'Succesfully Updated!',
         toastLength: Toast.LENGTH_LONG,
       );
     }
@@ -136,7 +163,8 @@ class Patient {
     return med;
   }
 
-  Future<int> addMedication(String _id, String _medicineId, String _token) async {
+  Future<int> addMedication(
+      String _id, String _medicineId, String _token) async {
     final url = Uri.https(domain, "/patients/$_id/medications");
 
     final headers = <String, String>{
@@ -160,12 +188,37 @@ class Patient {
       );
     } else {}
 
-    print(response.statusCode);
+    return response.statusCode;
+  }
+
+  Future<int> deleteMedication(
+      String _patientID, String _medicineId, String _token) async {
+    final url =
+        Uri.https(domain, "/patients/$_patientID/medications/$_medicineId");
+
+    final headers = <String, String>{
+      HttpHeaders.contentTypeHeader: ContentType.json.toString(),
+      HttpHeaders.acceptHeader: ContentType.json.toString(),
+      HttpHeaders.authorizationHeader: "Bearer $_token"
+    };
+
+    final response = await http.delete(
+      url,
+      headers: headers,
+    );
+
+    if (response.statusCode == HttpStatus.ok) {
+      Fluttertoast.showToast(
+        msg: 'Medicine Succesfully Deleted',
+        toastLength: Toast.LENGTH_LONG,
+      );
+    } else {}
 
     return response.statusCode;
   }
-  Future<List<Schedule>> getOneMedication(String _id, String _medId, String _token) async {
-    
+
+  Future<List<Schedule>> getOneMedication(
+      String _id, String _medId, String _token) async {
     final url = Uri.https(domain, "/patients/$_id/medications/$_medId");
 
     final headers = <String, String>{
@@ -180,12 +233,81 @@ class Patient {
 
     // data["schedules"] as List<dynamic>).map((e) => Schedule.fromJson(e)).toList();
 
-    final schedules = (data["schedules"] as List<dynamic>).map((e) => Schedule.fromJson(e)).toList();
-
-    print(schedules);
-
-    
+    final schedules = (data["schedules"] as List<dynamic>)
+        .map((e) => Schedule.fromJson(e))
+        .toList();
 
     return schedules;
+  }
+
+  Future<int> setSchedule(String _patientID, String _medicationID,
+      List<String> days, int time, String _token) async {
+    final url = Uri.https(
+        domain, "/patients/$_patientID/medications/$_medicationID/schedules");
+
+    final headers = <String, String>{
+      HttpHeaders.contentTypeHeader: ContentType.json.toString(),
+      HttpHeaders.acceptHeader: ContentType.json.toString(),
+      HttpHeaders.authorizationHeader: "Bearer $_token"
+    };
+
+    final response = await http.post(url,
+        headers: headers, body: jsonEncode({"days": days, "time": time}));
+
+    if (response.statusCode == HttpStatus.ok) {
+      Fluttertoast.showToast(
+        msg: 'Schedule Set!',
+        toastLength: Toast.LENGTH_LONG,
+      );
+    } else {}
+
+    return response.statusCode;
+  }
+
+  Future<int> deleteSchedule(String _patientID, String _medicationID,
+      String _schedID, String _token) async {
+    final url = Uri.https(domain,
+        "/patients/$_patientID/medications/$_medicationID/schedules/$_schedID");
+
+    final headers = <String, String>{
+      HttpHeaders.contentTypeHeader: ContentType.json.toString(),
+      HttpHeaders.acceptHeader: ContentType.json.toString(),
+      HttpHeaders.authorizationHeader: "Bearer $_token"
+    };
+
+    final response = await http.delete(url, headers: headers);
+
+    if (response.statusCode == HttpStatus.ok) {
+      Fluttertoast.showToast(
+        msg: 'Schedule Deleted Successfully!',
+        toastLength: Toast.LENGTH_LONG,
+      );
+    } else {}
+
+    return response.statusCode;
+  }
+
+  Future<int> editSchedule(String _patientID, String _medicationID,
+      String _schedID, List<String> _days, int _time, String _token) async {
+    final url = Uri.https(domain,
+        "/patients/$_patientID/medications/$_medicationID/schedules/$_schedID");
+
+    final headers = <String, String>{
+      HttpHeaders.contentTypeHeader: ContentType.json.toString(),
+      HttpHeaders.acceptHeader: ContentType.json.toString(),
+      HttpHeaders.authorizationHeader: "Bearer $_token"
+    };
+
+    final response = await http.patch(url,
+        headers: headers, body: jsonEncode({"days": _days, "time": _time}));
+
+    if (response.statusCode == HttpStatus.ok) {
+      Fluttertoast.showToast(
+        msg: 'Schedule Udated Successfully!',
+        toastLength: Toast.LENGTH_LONG,
+      );
+    } else {}
+
+    return response.statusCode;
   }
 }
